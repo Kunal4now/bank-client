c2600b3a1178882bac9dc422305d4d31000ac563
Kunal <kunal.rohitas29@gmail.com>
2022-07-15 10:11:11 +0530

    Fix protected route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,14 @@
 import {Navigate, Outlet} from 'react-router-dom';
-
+import UserContext from '../context/user/UserContext';
+import {useContext, useEffect} from 'react';
 const ProtectedRoute = ({auth}) => {
-    return (auth === true) ? <Outlet /> : <Navigate to="/" replace/>;
+    const context = useContext(UserContext);
+    const {user, getUser} = context;
+    useEffect(() => {
+      getUser()
+    }
+    , [])
+    return (user.role !== null) ? <Outlet /> : <Navigate to="/" replace/>;
 }
 
 export default ProtectedRoute;
